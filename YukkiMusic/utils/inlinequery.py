c47033a981c394d89fb5e676d31906397b9010d4
presetrend <103375708+presetrend@users.noreply.github.com>
2022-04-14 09:51:47 +0700

    Update inlinequery.py

--- YukkiMusic/utils/inlinequery.py
+++ YukkiMusic/utils/inlinequery.py
@@ -15,25 +15,25 @@ answer = []
 answer.extend(
     [
         InlineQueryResultArticle(
-            title="Jeda Streaming",
+            title="Pause Stream",
             description=f"Jeda pemutaran saat ini pada panggilan grup.",
             thumb_url="https://telegra.ph/file/c0a1c789def7b93f13745.png",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
-            title="Lanjutkan Streaming",
+            title="Resume Stream",
             description=f"Lanjutkan pemutaran yang sedang berlangsung pada panggilan grup.",
             thumb_url="https://telegra.ph/file/02d1b7f967ca11404455a.png",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
-            title="Bisukan Streaming",
+            title="Mute Stream",
             description=f"Bisukan pemutaran yang sedang berlangsung pada panggilan grup.",
             thumb_url="https://telegra.ph/file/66516f2976cb6d87e20f9.png",
             input_message_content=InputTextMessageContent("/mute"),
         ),
         InlineQueryResultArticle(
-            title="Bunyikan Streaming,
+            title="Unmute Stream,
             description=f"Suarakan pemutaran yang sedang berlangsung pada panggilan grup.",
             thumb_url="https://telegra.ph/file/3078794f9341ffd582e18.png",
             input_message_content=InputTextMessageContent("/unmute"),
@@ -45,25 +45,25 @@ answer.extend(
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
-            title="Akhiri Streaming",
+            title="End Stream",
             description="Hentikan pemutaran yang sedang berlangsung pada panggilan grup.",
             thumb_url="https://telegra.ph/file/d2eb03211baaba8838cc4.png",
             input_message_content=InputTextMessageContent("/stop"),
         ),
         InlineQueryResultArticle(
-            title="Streaming Acak",
+            title="Shuffle Stream",
             description="Acak daftar trek yang antri.",
             thumb_url="https://telegra.ph/file/7f6aac5c6e27d41a4a269.png",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
-            title="Cari Streaming",
+            title="Seek Stream",
             description="Cari streaming yang sedang berlangsung ke durasi tertentu.",
             thumb_url="https://telegra.ph/file/cd25ec6f046aa8003cfee.png",
             input_message_content=InputTextMessageContent("/seek 10"),
         ),
         InlineQueryResultArticle(
-            title="Streaming Putaran",
+            title="Loop Stream",
             description="Putar musik yang sedang diputar. | Penggunaan: /loop [aktifkan|nonaktifkan]",
             thumb_url="https://telegra.ph/file/081c20ce2074ea3e9b952.png",
             input_message_content=InputTextMessageContent("/loop 3"),
